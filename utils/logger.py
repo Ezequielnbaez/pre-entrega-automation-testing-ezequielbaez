fefeb9d0aaae043bb43cc4c36058355103c74b8a
Ezequiel Baez <ezequielnbaez@gmail.com>
2025-12-11 11:50:55 -0300

    logger funcionando

--- utils/logger.py
+++ utils/logger.py
@@ -1,15 +1,31 @@
 import logging
+import sys
 import os
 
-LOG_DIR = "logs"
+# crea carpeta si no existe
+LOG_FOLDER = "logs"
+os.makedirs(LOG_FOLDER, exist_ok=True)
 
-if not os.path.exists(LOG_DIR):
-    os.makedirs(LOG_DIR)
+LOG_FILE = os.path.join(LOG_FOLDER, "test.log")
 
-logging.basicConfig(
-    filename=f"{LOG_DIR}/ejecucion.log",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
-)
+logger = logging.getLogger("test_logger")
+logger.setLevel(logging.INFO)
 
-logger = logging.getLogger()
+# evita duplicar handlers
+if not logger.handlers:
+
+    # consola
+    console = logging.StreamHandler(sys.stdout)
+    console.setLevel(logging.INFO)
+    console_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    console.setFormatter(console_formatter)
+    logger.addHandler(console)
+
+    # archivo
+    file_handler = logging.FileHandler(LOG_FILE, mode="a", encoding="utf-8")
+    file_handler.setLevel(logging.INFO)
+    file_formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s"
+    )
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
